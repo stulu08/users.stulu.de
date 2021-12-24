76788bb7c8b38313399eba1f1752c530c1c24f9e
Stulu <58867625+stulu08@users.noreply.github.com>
2021-12-24 17:38:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ $userId = $_GET['user'];
 $userPw = $_GET['password'];
 if($userId == null || $userPw == null) {
 if($userId == null)
-echo "user invalid\\n";
+echo "user invalid\r\n";
 if($userPw == null)
-echo "password in invalid";
+echo "password in invalid\r\n";
 }
 else
 echo "session-id=67382827367483";
