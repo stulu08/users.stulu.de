4aeec5dee1838d4947ca7d94f759b7add8b4fc51
Stulu <58867625+stulu08@users.noreply.github.com>
2021-12-24 17:40:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $userId = $_GET['user'];
 $userPw = $_GET['password'];
-if($userId == null || $userPw == null) {
+if($userId == null || $userPw == null)
 echo "error: invalid-data";
 else
 echo "session-id: 67382827367483";
